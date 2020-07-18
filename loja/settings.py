09d81dd089085ebf2db4604be5e736fb13a210f9
adriano-ferrari <adriano.ferrari@aluno.faculdadeimpacta.com.br>
2020-07-18 17:54:06 -0300

    aula 210

--- loja/settings.py
+++ loja/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'produto',
 
     # TODO: Remover debug toolbar
     'debug_toolbar',
